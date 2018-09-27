98f8cb1db69784c9f1237fbdc2cf445af0c649f9
shaffe <mmik@sto-m-mmik.fritz.box>
2018-09-27 18:15:55 +0200

    about me email obfuscator contact me section in project pages :wq

--- js/main.js
+++ js/main.js
@@ -99,6 +99,30 @@ $(window).scroll(function() {
 });
 
 
+// Email obfuscator script 2.1 by Tim Williams, University of Arizona
+// Random encryption key feature coded by Andrew Moulden
+// This code is freeware provided these four comment lines remain intact
+// A wizard to generate this code is at http://www.jottings.com/obfuscator/
+{ coded = "vrFFE@7hZUI7UB8FUm.mE7"
+  key = "N0bAjJSQ7eVT9LlRUItBovyKuDcMPqgrY512FGpa6HExws83m4zdWZfCinOkXh"
+  shift=coded.length
+  link=""
+  for (i=0; i<coded.length; i++) {
+    if (key.indexOf(coded.charAt(i))==-1) {
+      ltr = coded.charAt(i)
+      link += (ltr)
+    }
+    else {     
+      ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+      link += (key.charAt(ltr))
+    }
+  }
+document.write("<a href='mailto:"+link+"'>"+link+"</a>")
+}
+
+
+
+
 
 
 
